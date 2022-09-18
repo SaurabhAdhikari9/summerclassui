7b5e39e8e2fd5e3e865db566ec75b44b4e59fc54
Saurabh Adhikari <sauravadhikari98@gmail.com>
2022-09-18 17:56:42 +0545

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard.jsx';
+import Analytics from './pages/Analytics.jsx';
+import Category from './pages/Category.jsx'
+import Orders from './pages/Order.jsx';
+import ProductList from './pages/ProductList.jsx';
+import Staff from './pages/Staff';
 function App() {
   return (
-    <>
-    <h1>
-      Hello world
-    </h1>
-    </>
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/category" element={<Category />} />
+          <Route path="/" element={<Analytics />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/orders" element={<Orders/>} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
 }
 
